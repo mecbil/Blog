78e706b072cbf11b107cfbcd223c5e480799bb56
Mecili <nabilmecili@gmail.com>
2021-04-27 01:58:24 +0200

    somme changes

---
+++ models/Comment.php
@@ -0,0 +1,11 @@
+<?php
+namespace Models;
+
+class Comment extends Model
+{
+    protected $table = "comments";
+};
+
+
+
+
---
+++ models/Database.php
@@ -0,0 +1,40 @@
+<?php
+// namespace models;
+
+require_once('Model.php');
+
+
+class Database
+{
+    private static $instance= null;
+
+    /**
+     * Connexion à la base de données
+     * 
+     */
+    
+
+    public static function dbconnect()
+    {
+        if (self::$instance===null)
+        {
+            try
+            {
+                self::$instance = new PDO ('mysql:host=localhost;dbname=mnblog;charset=utf8', 'root', '',
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO:: FETCH_ASSOC
+            ]);
+                
+            }
+            catch (Exception $e)
+            {
+                echo'<script language="Javascript"> alert ( "'. $e->getMessage() .'" )</script>';
+            }
+
+        }
+        
+        return self::$instance;
+    }
+}
+
