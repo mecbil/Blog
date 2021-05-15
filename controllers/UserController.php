9efe3fac70df123063c8a2ef29daa4dbf8fae1e0
Mecili <nabilmecili@gmail.com>
2021-05-15 22:07:34 +0200

    Getters and Setters

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,16 +3,29 @@ namespace Controllers ;
 
 require_once('../application/autoload.php');
 
+use Models\User;
+use Models\Post;
+use Models\Comment;
+
 class UserController
 {
     // Montrer la page de connection
-    public static function ShowConnect()
+    public static function showConnect()
     {
         if (\session_status() === PHP_SESSION_NONE) {
             session_start();
             if (isset($_SESSION['user'])) {
-                
                 $pageTitle = $_SESSION['user'];
+                
+                $modelpost= new Post();
+                $posts= $modelpost->findAll();
+
+                $modelcomment= new Comment();
+                $comments= $modelcomment->findAll();
+
+                $_SESSION['posts'] = $posts;
+                $_SESSION['comments'] = $comments;
+
                 \Application\Renderer::Render('users/indexuser', compact('pageTitle'));
             } else {
                 $pageTitle = "Connexion" ;
@@ -24,12 +37,16 @@ class UserController
     // Verifier les données d'un formulaire
     public function verif($donnees)
     {
+        // Si l'information exist
         if ($donnees) {
+            $donnees = str_replace('/', '', $donnees);
+            $donnees = strip_tags($donnees);
             $donnees = trim($donnees);
+            
             $donnees = stripslashes($donnees);
             $donnees = htmlspecialchars($donnees);
         } else {
-            $_SESSION['erreur']='Veuillez remplir tous les champs'.$_POST["email"];
+            $_SESSION['erreur']='Veuillez remplir tous les champs';
             
             $pageTitle = "Connexion" ;
             \Application\Renderer::render('users/connection', compact('pageTitle'));
@@ -41,39 +58,41 @@ class UserController
     // Montrer la page d'administration
     public function connect()
     {
-      
-      /**
-       * 1- verifier les informations
-       */
+        // 1- verifier les informations
         $email= $this->verif($_POST["email"]);
-        /**
-         * 2- connecter un utilisateur
-         */
-        $modeluser= new \Models\User();
-        $user= $modeluser->search('mail', $email);
-      
-        /**
-        * 2- Affichage (Show)
-        */
 
+        // 2- connecter un utilisateur
+        $modelUser= new User();
+        $user= $modelUser->search('mail', $email);
+
+        // 3- Affichage (Show)
+
+        // Si l'utilisateur avec le mail entré exist
         if ($user) {
+            $userExist = $modelUser->hydrate($user);
 
-            if (password_verify($_POST['password'], $user['password'] ) ) {
-                $pageTitle = $user["nickname"]." Admin";
+            // On verifie le mot de passe dans la table avec celui donné
+            if (password_verify($_POST['password'], $userExist->getPassword())) {
+                // Ici Mail et mots de passe exacte
+                $pageTitle = $user->nickname;
 
-                $modelpost= new \Models\Post();
+                $modelpost= new Post();
                 $posts= $modelpost->findAll();
-
-                $modelcomment= new \Models\Comment();
+                //$postsHydrate= $modelpost->hydrate($posts);
+                $modelcomment= new Comment();
                 $comments= $modelcomment->findAll();
-                
+                //$commentssHydrate= $modelcomment->hydrate($comments);
                 if (\session_status() === PHP_SESSION_NONE) {
                     session_start();
-                    $_SESSION['user'] = $user["nickname"];
-                    \Application\Renderer::render('users/indexuser', compact('pageTitle', 'user', 'posts', 'comments'));
+                    $_SESSION['user'] = $user->nickname;
+                    $_SESSION['rule'] = $user->Rule;
+                    $_SESSION['posts'] = $posts;
+                    $_SESSION['comments'] = $comments;
+                    \Application\Renderer::render('users/indexuser', compact('pageTitle', 'user'));
                 }
             } else {
-                $_SESSION['erreur']='Adresse mail ou mots de passe incorrect ';
+                // Utilisateur avec mail donné n'existe pas
+                $_SESSION['erreur']='Veuillez donnez les bons identifiant ou creer un nouveau compte';
                 $pageTitle = "Connexion";
 
                 \Application\Renderer::render('users/connection', compact('pageTitle'));
@@ -85,6 +104,7 @@ class UserController
             \Application\Renderer::render('users/connection', compact('pageTitle'));
         }
     }
+
     // Deconnecter l'utilisateur
     public function disconnect()
     {
--- models/Comment.php
+++ models/Comment.php
@@ -4,4 +4,124 @@ namespace Models;
 class Comment extends Model
 {
     protected $table = "comments";
-};
+    protected $id;
+    protected $uuid;
+    protected $posts_id;
+    protected $date;
+    protected $comment;
+    protected $title;
+    protected $author;
+    protected $users_id;
+
+    // Get the value of id
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    // Set the value of id
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    // Get the value of uuid
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    // Set the value of uuid
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    // Get the value of posts_id
+    public function getPosts_id()
+    {
+        return $this->posts_id;
+    }
+
+    // Set the value of posts_id
+    public function setPosts_id($posts_id)
+    {
+        $this->posts_id = $posts_id;
+
+        return $this;
+    }
+
+    // Get the value of date
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    // Set the value of date
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    // Get the value of comment
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    // Set the value of comment
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    // Get the value of title
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    // Set the value of title
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    // Get the value of author
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    // Set the value of author
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    // Get the value of users_id
+    public function getUsers_id()
+    {
+        return $this->users_id;
+    }
+
+    // Set the value of users_id
+    public function setUsers_id($users_id)
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+}
