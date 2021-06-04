1625de2cffcbfccb29f178f78c22f87b6e817eff
Mecili <nabilmecili@gmail.com>
2021-06-04 19:31:13 +0200

    add update post

--- views/users/indexuser.html.php
+++ views/users/indexuser.html.php
@@ -10,7 +10,15 @@
       </div>
       <?php endif; ?>
             
-      <form action="index.php?controller=Postcontroller&task=insertpost" method="post">
+      <form 
+      <?php if ($edit == false ): ?>
+        action="index.php?controller=Postcontroller&task=insertpost"
+      <?php endif; ?>
+      <?php if ($edit == true ): ?>
+        action="index.php?controller=Postcontroller&task=updatePost"
+      <?php endif; ?>
+
+      method="post">
           <h2 class="text-center">Ajout d'un Blog Post </h2>
           <div class="form-group">
             <input type="text" name="title" value="<?php if (isset($_POST['title'])){echo $_POST['title'];} ?>" class="form-control" placeholder="Title"  autocomplete="off">
@@ -19,13 +27,21 @@
             <input type="text" name="chapo" value="<?php if (isset($_POST['chapo'])){echo $_POST['chapo'];} ?>" class="form-control" placeholder="Chapo"  autocomplete="off">
           </div>
           <div class=" form-group ">
-            <textarea name="content" value="<?php if (isset($_POST['content'])) {echo $_POST['content'];} ?>" class="form-control" placeholder="Content ..."  autocomplete="off"></textarea>
+            <textarea name="content" value="" class="form-control" placeholder="Content ..."  autocomplete="off"><?php if (isset($_POST['content'])) {echo $_POST['content'];} ?></textarea>
           </div>
           <div class=" form-group ">
               <input type="text" name="author" value="<?php if (isset($_POST['author'])){echo $_POST['author'];} ?>" class="form-control" placeholder="Author"  autocomplete="off">
           </div>
           <div class=" form-group ">
-              <button type="submit" class="btn btn-dark btn-outline-light btn-block mt-2">Valider</button>
+              <input type="hidden" name="id" value="<?php if (isset($_POST['id'])){echo $_POST['id'];} ?>" class="form-control" >
+          </div>
+          <div class=" form-group ">
+            <?php if ($edit == false ): ?>
+            <button type="submit" class="btn btn-dark btn-outline-light btn-block mt-2">Valider</button>
+            <?php endif; ?>
+            <?php if ($edit == true ): ?>
+            <button type="submit" class="btn btn-dark btn-outline-light btn-block mt-2">Modifier</button>
+            <?php endif; ?>
           </div>
       </form>
     </div>
@@ -37,7 +53,7 @@
     <h2 class="text-center">Gestion :</h2>
     <form action="index.php?controller=Postcontroller&task=validposts" method="post">
       <div class="form-group">
-        <button type="submit" class="btn btn-dark btn-outline-light btn-block mt-2">Blog Post Non validé</button>
+        <button type="submit" class="btn btn-dark btn-outline-light btn-block mt-2">Commentaires Non validé</button>
       </div>
       <div class="valider">
         <?php foreach ($comments as $comment) : ?>
