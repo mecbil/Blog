b60ae35cc6ca8d737f66ff89588ef9c512f14443
Mecili <nabilmecili@gmail.com>
2021-06-08 23:24:03 +0200

    some changes for codacy6

--- application/Site.php
+++ application/Site.php
@@ -7,29 +7,42 @@ class Site
 {
     public static function frontControl()
     {
-        if (empty($_GET['controller']) && $_SERVER['REQUEST_URI'] !='/') {
-            header('Location: /404.php');
-            // var_dump($_SERVER['SERVER_NAME'].'/404.php');
-        }
+        $getcontroller = isset($_GET['controller']) ? filter_var($_GET['controller'], FILTER_SANITIZE_STRING) :"";
+        //$server = $_SERVER['REQUEST_URI'];
+        $gettask = isset($_GET['task']) ? filter_var($_GET['task'], FILTER_SANITIZE_STRING) :"";
+
+        // if (empty($get) && $_SERVER['REQUEST_URI'] !='/') {
+        //     header('Location: /404.php');
+        // }
+        // Controller par default MainController
         $controllerUse = 'MainController';
-        if (!empty($_GET['controller'])) {
-            $controllerUse = ucfirst($_GET['controller']);
+
+        if (!empty($getcontroller)) {
+            $controllerUse = ucfirst($getcontroller);
         }
 
         //Tache par default showindex
         $task="showIndex";
 
-        if (!empty($_GET['task'])) {
-            $task = $_GET['task'];
+        if (!empty($gettask)) {
+            $task = $gettask;
         }
+
 
         $controllerUse = "Controllers\\" . $controllerUse;
 
         if (is_file('..\\'.$controllerUse.'.php')) {
             $controller = new $controllerUse();
-            $controller->$task();
-        }else {
+
+            if (method_exists($controllerUse, $task)) {
+                $controller->$task();
+            } 
+            if (!method_exists($controllerUse, $task)) {
+                header('Location: /404.php');
+            }
+        }
+        if (!is_file('..\\'.$controllerUse.'.php')) {
             header('Location: /404.php');
-        } 
+        }
     }
 }
