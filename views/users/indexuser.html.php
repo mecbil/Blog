230dd2d8be659244647cadbcc692e917b88e3f52
Mecili <nabilmecili@gmail.com>
2021-06-14 13:06:23 +0200

    securites

--- views/users/indexuser.html.php
+++ views/users/indexuser.html.php
@@ -2,11 +2,11 @@
 
   <div class="text-light col-11 col-lg-5 col-md-9 col-sm-11 pt-2">
     <!-- zone de Ajout Post blog -->
-    <h1>Dashbord <?= htmlspecialchars($_SESSION['user']) ?></h1>
+    <h1>Dashbord <?= strip_tags($_SESSION['user']) ?></h1>
     <div class="">
       <?php if (!empty($erreur)): ?>
       <div class="alert alert-danger">
-        <?= htmlspecialchars($erreur) ?>
+        <?= strip_tags($erreur) ?>
       </div>
       <?php endif; ?>
             
