0848fc71f9b92bea1642f62346110e5725537e6e
Mecili <nabilmecili@gmail.com>
2021-06-16 20:08:40 +0200

    changes neaded 4

--- views/Posts/posts.html.php
+++ views/Posts/posts.html.php
@@ -1,30 +1,25 @@
 <div class="row" >
-<?php foreach ($posts as $article) : ?>
+    <?php foreach ($posts as $article) : ?>
     <div class="col-lg-4" id="carte">
         <div class="card border-light mb-4">
-            <div class="card-header text-light bg-success text-center"><?= htmlspecialchars($article->getTitle()) ?> </div>
-            <p class="text-center"> Publié le :
-                <?php $datefr= strtotime(htmlspecialchars($article->getDate_modify())); echo(date('d-m-Y H:i:s', $datefr)) ?></p>
+            <div class="card-header text-light bg-success text-center"><?= $article->getTitle() ?></div>
+            <p class="text-center"> Publié le : <?php $datefr= strtotime($article->getDate_modify()); echo(date('d-m-Y H:i:s', $datefr)) ?></p>
             <div class="card-body text-primary m-0 p-0">
-                <h5 class="card-title"><?php
-                  if (strlen(htmlspecialchars($article->getChapo()))>=70) {
-                      echo substr(htmlspecialchars($article->getChapo()), 0, 70). "...";
-                  } else {
-                      echo substr(htmlspecialchars($article->getChapo()), 0, 70);
-                  }
-                  ?></h5>
-                <p class="text-secondary">
-                <p>Modifié Le : <?= htmlspecialchars($article->getDate_modify()) ?></p>
-                </p>
+                <h5 class="card-title">
+                    <?php
+                    if (strlen($article->getChapo())>=70) {
+                        echo substr($article->getChapo(), 0, 70). "...";
+                    } else {
+                        echo substr($article->getChapo(), 0, 70);
+                    }
+                    ?>
+                </h5>
+                <p class="text-secondary">Modifié Le : <?= $article->getDate_modify() ?></p>
             </div>
             <div class="card-footer bg-success text-center pb-0">
-                <p><a class="btn btn-outline-light btn-dark"
-                        href="/?controller=postcontroller&task=showOnePost&uuid=<?= htmlspecialchars($article->getUuid()) ?>">Détails &raquo;</a>
-                </p>
+                <p><a class="btn btn-outline-light btn-dark" href="/?controller=postcontroller&task=showOnePost&uuid=<?= $article->getUuid() ?>">Détails &raquo;</a></p>
             </div>
-
         </div>
     </div>
     <?php endforeach ?>
-  
 </div>
--- views/index.html.php
+++ views/index.html.php
@@ -10,8 +10,7 @@
     <div class="col-lg-4" id="carte">
         <div class="card border-light mb-4">
             <div class="card-header text-light bg-success text-center"><?= $article->getTitle() ?> </div>
-            <p class="text-center"> Publié le :
-                <?php $datefr= strtotime($article->getDate_modify()); echo(date('d-m-Y H:i:s', $datefr)) ?></p>
+            <p class="text-center"> Publié le : <?php $datefr= strtotime($article->getDate_modify()); echo(date('d-m-Y H:i:s', $datefr)) ?></p>
             <div class="card-body text-primary m-0 p-0">
                 <h5 class="card-title"><?php
                   if (strlen($article->getChapo())>=70) {
@@ -25,9 +24,7 @@
                 </p>
             </div>
             <div class="card-footer bg-success text-center pb-0">
-                <p><a class="btn btn-outline-light btn-dark"
-                        href="/?controller=postcontroller&task=showOnePost&uuid=<?= $article->getUuid() ?>">Détails &raquo;</a>
-                </p>
+                <p><a class="btn btn-outline-light btn-dark" href="/?controller=postcontroller&task=showOnePost&uuid=<?= $article->getUuid() ?>">Détails &raquo;</a></p>
             </div>
         </div>
     </div>
