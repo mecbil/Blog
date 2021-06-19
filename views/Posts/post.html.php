97b91fc815cabfef8dd2f89668c44ccf9a75fc87
Mecili <nabilmecili@gmail.com>
2021-06-19 18:28:00 +0200

    output error in codacy5

--- views/Posts/post.html.php
+++ views/Posts/post.html.php
@@ -1,10 +1,10 @@
 <div class="text-light">
     <div class="container m-2">
-        <h1><?php print($post->getTitle()) ?></h1>
-        <small>Publié le : <?php $datef= strtotime($post->getDate_creat()); print(date('d-m-Y'.' à '.' H:i:s', $datef)) ?></small>
-        <p><?php print($post->getChapo()) ?></p>
-        <p><?php print(nl2br($post->getContent())) ?></p>
-        <div >Auteur : <small class="text-warning"><?php print($post->getAuthor()) ?></small></div>
+        <h1><?php print_r($post->getTitle()) ?></h1>
+        <small>Publié le : <?php $datef= strtotime($post->getDate_creat()); print_r(date('d-m-Y'.' à '.' H:i:s', $datef)) ?></small>
+        <p><?php print_r($post->getChapo()) ?></p>
+        <p><?php print_r(nl2br($post->getContent())) ?></p>
+        <div >Auteur : <small class="text-warning"><?php print_r($post->getAuthor()) ?></small></div>
         <?php if (!isset($_SESSION['user'])): ?>
             <h3>Veuillez vous connecter pour réagir !</h3>
             <?php endif; ?>
@@ -17,19 +17,19 @@
 
 <?php if (!empty($erreur)): ?>
 <div class="alert alert-danger">
-<?php print($erreur) ?>
+<?php print_r($erreur) ?>
 </div>
 <?php endif; ?>
 <?php if (isset($_SESSION['user'])): ?>
     <form class="m-2" action="index.php?controller=Commentcontroller&task=insertComment" method="POST">
         <h3>Réagir ? N'hésitez pas !</h3>
-        <input type="text" name="pseudo" value ="<?php if (isset($_SESSION['user'])){print($_SESSION['user']);} ?>"" placeholder="Votre pseudo !">
+        <input type="text" name="pseudo" value ="<?php if (isset($_SESSION['user'])){print_r($_SESSION['user']);} ?>"" placeholder="Votre pseudo !">
         <br>
         <textarea name="comment" cols="30" rows="2" placeholder="Votre commentaire ..."></textarea>
         <br>
-        <input type="hidden" name="post_id" value ="<?php print($post->getPost_id()) ?>">
-        <input type="hidden" name="user_id" value ="<?php print($_SESSION['user_id']) ?>">
-        <input type="hidden" name="uuid" value ="<?php print($post->getUuid()) ?>">
+        <input type="hidden" name="post_id" value ="<?php print_r($post->getPost_id()) ?>">
+        <input type="hidden" name="user_id" value ="<?php print_r($_SESSION['user_id']) ?>">
+        <input type="hidden" name="uuid" value ="<?php print_r($post->getUuid()) ?>">
         <br>
         <button type="submit" class="btn btn-dark btn-outline-light">Commenter !</button>
     </form>
@@ -41,16 +41,16 @@
         <h2>Il y a déjà <?= count($comments) ?> réactions : </h2>
         <?php foreach ($comments as $commentaire) : ?>
             <h3>Commentaire de : <?php print_r($commentaire->getAuthor()) ?></h3>
-            <small>Le <?php print($commentaire->getDate_modify()) ?></small>
+            <small>Le <?php print_r($commentaire->getDate_modify()) ?></small>
             <blockquote>
-                <em><?php print(nl2br($commentaire->getComment())) ?></em>
+                <em><?php print_r(nl2br($commentaire->getComment())) ?></em>
             </blockquote>
             <?php if (isset($_SESSION['user']) && $_SESSION['role'] == true): ?> 
-                <a class="btn btn-danger btn-outline-light" href="/?controller=commentcontroller&task=deleteComment&uuid=<?php print($post->getUuid()) ?>&commentid=<?php print($commentaire->getComment_id()) ?>" onclick="return window.confirm(`Êtes vous sûr de vouloir supprimer ce commentaire ?!`)">Supprimer</a>
+                <a class="btn btn-danger btn-outline-light" href="/?controller=commentcontroller&task=deleteComment&uuid=<?php print_r($post->getUuid()) ?>&commentid=<?php print_r($commentaire->getComment_id()) ?>" onclick="return window.confirm(`Êtes vous sûr de vouloir supprimer ce commentaire ?!`)">Supprimer</a>
                 <a class="btn btn-secoundary btn-outline-light" href="/?controller=commentcontroller&task=deleteComment&uuid=<?= filter_input(INPUT_GET, 'uuid') ?>" tabindex="-1">Editer</a>
             <?php endif; ?>
             <?php if (isset($_SESSION['user'])  && $_SESSION['role'] == false && ($_SESSION['user_id'] === $commentaire->getUser_id() )): ?>
-                <a class="btn btn-danger btn-outline-light" href="/?controller=commentcontroller&task=deleteComment&uuid=<?php print($commentaire->getUuid()) ?>" onclick="return window.confirm(`Êtes vous sûr de vouloir supprimer ce commentaire ?!`)">Supprimer</a>
+                <a class="btn btn-danger btn-outline-light" href="/?controller=commentcontroller&task=deleteComment&uuid=<?php print_r($commentaire->getUuid()) ?>" onclick="return window.confirm(`Êtes vous sûr de vouloir supprimer ce commentaire ?!`)">Supprimer</a>
             <?php endif; ?>
         <?php endforeach ?>
     <?php endif ?>
