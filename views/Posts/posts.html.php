8c8714c9c1d1ba19e1ed401b70318ff0ff5f3b21
Mecili <nabilmecili@gmail.com>
2021-06-23 08:25:29 +0200

    change print to echo

--- views/Posts/posts.html.php
+++ views/Posts/posts.html.php
@@ -2,7 +2,7 @@
     <?php foreach ($posts as $article) : ?>
     <div class="col-lg-4" id="carte">
         <div class="card border-light mb-0">
-            <div class="card-header text-light bg-success text-center"><?php print_r($article->getTitle()) ?></div>
+            <div class="card-header text-center"><?php print_r($article->getTitle()) ?></div>
             <p class="text-center"> Modifié le : <?php $datefr= strtotime($article->getDate_modify()); print_r(date('d-m-Y H:i:s', $datefr)) ?></p>
             <div class="card-body">
                 <h5 class="card-title">
@@ -15,7 +15,7 @@
                     ?>
                 </h5>
             </div>
-            <div class="card-footer bg-success text-center pb-0">
+            <div class="card-footer text-center pb-0">
                 <p><a class="btn btn-outline-light btn-dark" href="/?controller=postcontroller&task=showOnePost&uuid=<?php print_r($article->getUuid()) ?>">Détails &raquo;</a></p>
             </div>
         </div>
--- views/index.html.php
+++ views/index.html.php
@@ -6,15 +6,15 @@
 </div>
 
 <div class="row container-fluid">
-    <h2>Derinières publications</h2>
+    <h2>Dernières publications</h2>
 
     <?php foreach ($posts as $article) : ?>
     <div class="col-lg-4" id="carte">
         <div class="card border-light">
-            <div class="card-header text-light bg-success text-center"><?php print_r($article->getTitle())  ?> </div>
+            <div class="card-header text-center"><?php print_r($article->getTitle())  ?></div>
             <p class="text-center"> Modifié le : <?php $datefr= strtotime($article->getDate_modify()); print_r(date('d-m-Y H:i:s', $datefr)) ?></p>
             <div class="card-body">
-                <h5 class="card-title"><?php
+                <h5><?php
                   if (strlen($article->getChapo())>=70) {
                       print_r(substr($article->getChapo(), 0, 70). "...") ;
                   } else {
@@ -22,7 +22,7 @@
                   }
                   ?></h5>
             </div>
-            <div class="card-footer bg-success text-center pb-0">
+            <div class="card-footer text-center pb-0">
                 <p><a class="btn btn-outline-light btn-dark" href="/?controller=postcontroller&task=showOnePost&uuid=<?php print_r($article->getUuid())  ?>">Détails &raquo;</a></p>
             </div>
         </div>
